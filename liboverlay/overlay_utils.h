1c6dc33ceeb01ba9035c4fc73c295d794cad7427
guoyin.chen <guoyin.chen@freescale.com>
2010-03-30 10:39:06 -0400

    Set threshold for queue buffer to accelerate display speed Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- liboverlay/overlay_utils.h
+++ liboverlay/overlay_utils.h
@@ -1,4 +1,18 @@
-
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef __OVERLAY_UTILS_H__
 #define __OVERLAY_UTILS_H__
@@ -18,6 +32,8 @@
 
 #define MAX_OVERLAY_INPUT_W 1280
 #define MAX_OVERLAY_INPUT_H 720
+
+#define OVERLAY_QUEUE_THRESHOLD 2
 
 #define FULL_TRANSPARANT_VALUE 255
 
