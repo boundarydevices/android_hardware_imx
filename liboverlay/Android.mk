44b80357693d21c0c4b999d7f3d07bda9eaedeb5
guoyin.chen <guoyin.chen@freescale.com>
2010-04-21 17:20:49 +0800

    Change soc type to IMX5X for mx51,mx53 Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- liboverlay/Android.mk
+++ liboverlay/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(BOARD_SOC_TYPE),IMX51)
+ifeq ($(BOARD_SOC_TYPE),IMX5X)
 
 LOCAL_PATH := $(call my-dir)
 
