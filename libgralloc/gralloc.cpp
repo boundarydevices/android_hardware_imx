50ab79e8339302a92c085ee1cafd500f3346f726
Kevin Yang <kevin.yang@freescale.com>
2010-05-05 10:56:55 +0800

    add pixel alignment handling for frame buffer allocation

--- libgralloc/gralloc.cpp
+++ libgralloc/gralloc.cpp
@@ -130,7 +130,7 @@ static int gralloc_alloc_framebuffer_locked(alloc_device_t* dev,
 
     const uint32_t bufferMask = m->bufferMask;
     const uint32_t numBuffers = m->numBuffers;
-    const size_t bufferSize = m->finfo.line_length * m->info.yres;
+    const size_t bufferSize = m->finfo.line_length * ALIGN_PIXEL(m->info.yres);
     if (numBuffers == 1) {
         // If we have only one buffer, we never use page-flipping. Instead,
         // we return a regular buffer which will be memcpy'ed to the main
@@ -383,8 +383,8 @@ static int gralloc_alloc(alloc_device_t* dev,
                 return -EINVAL;
         }
     } else {
-        alignedw = (w + 31) & ~31; 
-        alignedh = (h + 31) & ~31;
+        alignedw = ALIGN_PIXEL(w);
+        alignedh = ALIGN_PIXEL(h);
         int bpp = 0;
         switch (format) {
             case HAL_PIXEL_FORMAT_RGBA_8888:
@@ -432,7 +432,7 @@ static int gralloc_free(alloc_device_t* dev,
         // free this buffer
         private_module_t* m = reinterpret_cast<private_module_t*>(
                 dev->common.module);
-        const size_t bufferSize = m->finfo.line_length * m->info.yres;
+        const size_t bufferSize = m->finfo.line_length * ALIGN_PIXEL(m->info.yres);
         int index = (hnd->base - m->framebuffer->base) / bufferSize;
         m->bufferMask &= ~(1<<index); 
     } else { 
