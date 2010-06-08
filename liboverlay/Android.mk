b6ad40de911b9138a4031cdfa8ac1520473edaae
b07211 <b07211@freescale.com>
2010-06-08 13:50:19 -0400

    ENGR00124208 [Android MX508]Remove the IPU and GPU so build for mx508 board config
    Signed-off-by: b07211 <b07211@freescale.com>

--- liboverlay/Android.mk
+++ liboverlay/Android.mk
@@ -2,8 +2,7 @@
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
+# You may obtain a copy of the License at #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
@@ -13,6 +12,7 @@
 # limitations under the License.
 
 ifeq ($(BOARD_SOC_CLASS),IMX5X)
+ifeq ($(HAVE_FSL_IMX_IPU),true)
 
 LOCAL_PATH := $(call my-dir)
 
@@ -60,5 +60,5 @@ LOCAL_MODULE:= mxc-test-overlays
 LOCAL_MODULE_TAGS := tests
 
 include $(BUILD_EXECUTABLE)
-
+endif
 endif
