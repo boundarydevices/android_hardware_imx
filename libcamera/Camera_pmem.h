70b9dc05f599984ed97e2f94b5554861f8f67798
Yaoming Qin <b10945@freescale.com>
2011-03-22 11:27:19 +0800

    ENGR00140932 Add the copy right
    add the copyright
    
    Signed-off-by: Yaoming Qin<B10945@freescale.com>

--- libcamera/Camera_pmem.h
+++ libcamera/Camera_pmem.h
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Copyright 2009-2011 Freescale Semiconductor, Inc. All Rights Reserved.
+ */
+
 #ifndef __CAMERA_MEM__H__
 #define __CAMERA_MEM__H__
 
