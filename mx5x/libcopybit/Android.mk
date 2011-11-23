5726bea7c4c1a7f2282760ed9f3dc12f3483d446
b07117 <b07117@freescale.com>
2011-11-24 00:43:44 +0800

    ENGR00000000 fixing build error for mx5x ICS
    Copybit HAL is removed in ICS, disable mx5x copybit
    Removing overlay header in mx5x hwcomposer
    
    Signed-off-by: xianzhong <b07117@freescale.com>

--- mx5x/libcopybit/Android.mk
+++ mx5x/libcopybit/Android.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq (true, false)
 ifeq ($(HAVE_FSL_IMX_GPU),true)
 ifeq ($(BOARD_SOC_CLASS),IMX5X)
 
@@ -40,5 +41,6 @@ LOCAL_MODULE_TAGS := eng
 
 include $(BUILD_SHARED_LIBRARY)
 
+endif
 endif
 endif
