f2bb63b92f8fd0ecfe3d57d60dde73a45887b621
B38613 <B38613@freescale.com>
2012-03-05 16:32:53 +0800

    ENGR00174615:Enable AH-1613 GPS module
    modify Android.mk to compile libgps on IMX6.
    
    Signed-off-by: Zhou Jianzheng <B38613@freescale.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
-common_imx_dirs := libsensors alsa
-mx5x_dirs := $(common_imx_dirs) libgps mx5x/libcopybit mx5x/libgralloc  mx5x/hwcomposer mx5x/libcamera
+common_imx_dirs := libsensors alsa libgps
+mx5x_dirs := $(common_imx_dirs) mx5x/libcopybit mx5x/libgralloc  mx5x/hwcomposer mx5x/libcamera
 mx6_dirs := $(common_imx_dirs) mx6/libgralloc_wrapper mx6/hwcomposer mx6/libcamera
 
 ifeq ($(TARGET_BOARD_PLATFORM),imx6)
