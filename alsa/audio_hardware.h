88fa0cac58b653980a06c12c8cacf25cd18a130a
b02247 <b02247@freescale.com>
2012-04-05 16:59:51 +0800

    ENGR00179411 support tinyalsa
    base tinyalsa for sabresd_reva & sabresd_revb
    other board is base on alsa lib
    
    Signed-off-by: b02247 <b02247@freescale.com>

---
+++ alsa/audio_hardware.h
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/* Copyright (C) 2012 Freescale Semiconductor, Inc. */
+
+#ifndef ANDROID_INCLUDE_IMX_AUDIO_HARDWARE_H
+#define ANDROID_INCLUDE_IMX_AUDIO_HARDWARE_H
+
+#include <stdlib.h>
+#include <tinyalsa/asoundlib.h>
+
+#include <hardware/hardware.h>
+
+struct mixer_ctls
+{
+	struct mixer_ctl *headset_enable;
+    struct mixer_ctl *headset_volume;
+	struct mixer_ctl *speaker_enable;
+    struct mixer_ctl *speaker_volume;
+	struct mixer_ctl *earpiece_enable;
+    struct mixer_ctl *earpiece_volume;
+};
+
+struct imx_audio_device {
+    struct audio_hw_device hw_device;
+
+    pthread_mutex_t lock;       /* see note below on mutex acquisition order */
+    struct mixer *mixer;
+    struct mixer_ctls mixer_ctls;
+    int mode;
+    int devices;
+    struct pcm *pcm_modem_dl;
+    struct pcm *pcm_modem_ul;
+    int in_call;
+    float voice_volume;
+    struct imx_stream_in *active_input;
+    struct imx_stream_out *active_output;
+    bool mic_mute;
+    int tty_mode;
+    struct echo_reference_itfe *echo_reference;
+    bool bluetooth_nrec;
+    bool device_is_imx;
+    int wb_amr;
+    bool low_power;
+    int  main_card;
+    int  vice_card;
+};
+
+struct imx_stream_out {
+    struct audio_stream_out stream;
+
+    pthread_mutex_t lock;       /* see note below on mutex acquisition order */
+    struct pcm_config config;
+    struct pcm *pcm;
+    struct resampler_itfe *resampler;
+    char *buffer;
+    int standby;
+    struct echo_reference_itfe *echo_reference;
+    struct imx_audio_device *dev;
+    int write_threshold;
+    bool low_power;
+    int frame_count;
+    int write_flags;
+};
+
+#define MAX_PREPROCESSORS 3 /* maximum one AGC + one NS + one AEC per input stream */
+
+struct imx_stream_in {
+    struct audio_stream_in stream;
+
+    pthread_mutex_t lock;       /* see note below on mutex acquisition order */
+    struct pcm_config config;
+    struct pcm *pcm;
+    int device;
+    struct resampler_itfe *resampler;
+    struct resampler_buffer_provider buf_provider;
+    int16_t *buffer;
+    size_t frames_in;
+    unsigned int requested_rate;
+    int standby;
+    int source;
+    struct echo_reference_itfe *echo_reference;
+    bool need_echo_reference;
+    effect_handle_t preprocessors[MAX_PREPROCESSORS];
+    int num_preprocessors;
+    int16_t *proc_buf;
+    size_t proc_buf_size;
+    size_t proc_frames_in;
+    int16_t *ref_buf;
+    size_t ref_buf_size;
+    size_t ref_frames_in;
+    int read_status;
+
+    struct imx_audio_device *dev;
+};
+
+#endif  /* ANDROID_INCLUDE_IMX_AUDIO_HARDWARE_H */
---
+++ alsa/imx_audio_hal.cpp
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/* Copyright (C) 2012 Freescale Semiconductor, Inc. */
+
+#define LOG_TAG "legacy_audio_hw_hal"
+//#define LOG_NDEBUG 0
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <hardware/hardware.h>
+#include <system/audio.h>
+#include <hardware/audio.h>
+
+#include <cutils/log.h>
+#include <cutils/properties.h>
+
+namespace android_audio_legacy {
+
+extern "C" {
+
+#define AUDIO_HARDWARE_MODULE_ID_LEGACY   "audio.legacy"
+#define AUDIO_HARDWARE_MODULE_ID_SABRESD_REVA  "audio.sabresd_reva"
+#define AUDIO_HARDWARE_MODULE_ID_SABRESD_REVB  "audio.sabresd_revb"
+
+struct imx_audio_module {
+    struct audio_module module;
+};
+
+
+static int imx_adev_open(const hw_module_t* module, const char* name,
+                            hw_device_t** device)
+{
+    int ret = 0;
+    const hw_module_t       *module_audio;
+    char value[PROPERTY_VALUE_MAX];
+    bool found = false;
+
+    /*find the audio hal for different board*/
+    property_get("ro.product.device", value, "");
+
+    if(!strcmp(value, "sabresd_6q")) {
+        ret = hw_get_module(AUDIO_HARDWARE_MODULE_ID_SABRESD_REVB, &module_audio);
+        if(ret)   found = false;
+        else {
+            ret = module_audio->methods->open(module, AUDIO_HARDWARE_INTERFACE,(struct hw_device_t**)device);
+            if(ret)  found = false;
+            else     found = true;
+        }
+    }
+
+    if(!found && !strcmp(value, "sabresd_6q")) {
+        ret = hw_get_module(AUDIO_HARDWARE_MODULE_ID_SABRESD_REVA, &module_audio);
+        if(ret)   found = false;
+        else {
+            ret = module_audio->methods->open(module, AUDIO_HARDWARE_INTERFACE,(struct hw_device_t**)device);
+            if(ret)  found = false;
+            else     found = true;
+        }
+    }
+
+    if(!found) {
+        LOGW("reload the legacy audio hal");
+        ret = hw_get_module(AUDIO_HARDWARE_MODULE_ID_LEGACY, &module_audio);
+        if(ret)
+            goto out;
+
+        ret = module_audio->methods->open(module, AUDIO_HARDWARE_INTERFACE,(struct hw_device_t**)device);
+        if(ret)
+            goto out;
+    }
+
+out:
+    return ret;
+}
+
+static struct hw_module_methods_t imx_audio_module_methods = {
+        open: imx_adev_open
+};
+
+struct imx_audio_module HAL_MODULE_INFO_SYM = {
+    module: {
+        common: {
+            tag: HARDWARE_MODULE_TAG,
+            version_major: 1,
+            version_minor: 0,
+            id: AUDIO_HARDWARE_MODULE_ID,
+            name: "LEGACY Audio HW HAL",
+            author: "The Android Open Source Project",
+            methods: &imx_audio_module_methods,
+            dso : NULL,
+            reserved : {0},
+        },
+    },
+};
+
+}; // extern "C"
+
+}; // namespace android_audio_legacy
