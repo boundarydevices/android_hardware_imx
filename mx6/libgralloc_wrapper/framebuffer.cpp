ff5f9da2bd84bda55ef2497733cc50f3bfb55062
Xinyu Chen <xinyu.chen@freescale.com>
2012-04-26 11:20:52 +0800

    ENGR00180865-2 hal: clean the HAL codes
    Remove the unused libcamera dir
    Remove the unused mx6/libgralloc dir
    Remove the pmem allocator in mx6 camera HAL
    Remove the PMEM codes from mx6/
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- mx6/libgralloc_wrapper/framebuffer.cpp
+++ mx6/libgralloc_wrapper/framebuffer.cpp
@@ -57,6 +57,10 @@
 // numbers of buffers for page flipping
 #define NUM_BUFFERS 3
 
+inline size_t roundUpToPageSize(size_t x) {
+    return (x + (PAGE_SIZE-1)) & ~(PAGE_SIZE-1);
+}
+
 enum {
     PAGE_FLIP = 0x00000001,
     LOCKED = 0x00000002
@@ -500,7 +504,7 @@ static int mapFrameBufferWithParamLocked(struct private_module_t* module, struct
     int err;
     size_t fbSize = roundUpToPageSize(finfo.line_length * info.yres_virtual);
     module->framebuffer = new private_handle_t(dup(fd), fbSize,
-            private_handle_t::PRIV_FLAGS_USES_PMEM);
+            private_handle_t::PRIV_FLAGS_USES_DRV);
 
     module->numBuffers = info.yres_virtual / ALIGN_PIXEL_128(info.yres);
     module->bufferMask = 0;
