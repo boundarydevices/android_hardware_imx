d0892ed674f934c2772f0c518e6f3552e39baa52
Xiaowen Liu <b37945@freescale.com>
2012-06-26 10:16:46 +0800

    ENGR00215050 add action mode and refine multi-display code.
    add action mode and refine multi-display code.
    xml tool used to replace file read and write
    to save primary display parameters.
    
    Signed-off-by: Xiaowen Liu <b37945@freescale.com>

--- mx6/libgralloc_wrapper/Android.mk
+++ mx6/libgralloc_wrapper/Android.mk
@@ -21,7 +21,7 @@ ifeq ($(TARGET_HAVE_IMX_GRALLOC),true)
 include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils libGLESv1_CM libipu libhardware libutils
+LOCAL_SHARED_LIBRARIES := liblog libcutils libGLESv1_CM libipu libhardware libutils libfsl_xmltool
 
 LOCAL_SRC_FILES := 	\
 	gralloc.cpp 	\
@@ -31,6 +31,7 @@ LOCAL_SRC_FILES := 	\
 
 LOCAL_MODULE := gralloc.$(TARGET_BOARD_PLATFORM)
 LOCAL_CFLAGS:= -DLOG_TAG=\"$(TARGET_BOARD_PLATFORM).gralloc\" -D_LINUX
+LOCAL_C_INCLUDES = external/expat/lib
 
 
 #ifeq ($(HAVE_FSL_EPDC_FB),true)
@@ -45,3 +46,13 @@ LOCAL_MODULE_TAGS := eng
 
 include $(BUILD_SHARED_LIBRARY)
 endif
+
+
+####build xmltool lib######
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := liblog libcutils libutils libexpat
+LOCAL_C_INCLUDES = external/expat/lib
+LOCAL_SRC_FILES := XmlTool.cpp
+LOCAL_MODULE := libfsl_xmltool
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
