d88b86289aaed5713a44f204a9c07a1e192fd798
Xinyu Chen <xinyu.chen@freescale.com>
2012-09-20 14:56:00 +0800

    ENGR00225144 lights: Add lights to Makefile
    Add lights HAL to top Makefile.
    Remove the deprecated PRELINK define.
    
    Change-Id: I3ef0a484ada836c6e47a3ae456d86af76383c585
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- lights/Android.mk
+++ lights/Android.mk
@@ -17,7 +17,6 @@ LOCAL_PATH := $(call my-dir)
 
 # HAL module implemenation, not prelinked and stored in hw/
 include $(CLEAR_VARS)
-LOCAL_PRELINK_MODULE := true
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_SRC_FILES := fsl_lights.c
