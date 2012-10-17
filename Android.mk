e9fb624d5fa794b192cac1dfafeaf598a1bfd658
Oliver Brown <oliver.brown@freescale.com>
2012-10-17 16:36:00 -0500

    ENGR00230124 - Need to add audio HAL for JB
    Need to add basic audio HAL for Android Jelly Bean.
    
    Signed-off-by: Oliver Brown <oliver.brown@freescale.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
-common_imx_dirs := libsensors alsa libgps lights wlan power
-mx5x_dirs := $(common_imx_dirs) mx5x/libcopybit mx5x/libgralloc  mx5x/hwcomposer mx5x/libcamera
-mx6_dirs := $(common_imx_dirs) mx6/libgralloc_wrapper mx6/hwcomposer mx6/libcamera
+common_imx_dirs := libsensors libgps lights wlan power
+mx5x_dirs := $(common_imx_dirs) mx5x/audio mx5x/libcopybit mx5x/libgralloc  mx5x/hwcomposer mx5x/libcamera
+mx6_dirs := $(common_imx_dirs) alsa mx6/libgralloc_wrapper mx6/hwcomposer mx6/libcamera
 
 ifeq ($(TARGET_BOARD_PLATFORM),imx6)
   include $(call all-named-subdir-makefiles,$(mx6_dirs))
