7d2a669640266e1ea1d054d868b29531036679f8
b02247 <b02247@freescale.com>
2012-12-13 10:54:45 +0800

    ENGR00236896 refine the input stream
    1.add stereo to mono and mono to stereo convert in hal
    2.support usb mic plugin&plugout
    
    Change-Id: I3148aa8d7aa6c49ef52830b401aa66bc3fdf78c8
    Signed-off-by: b02247 <b02247@freescale.com>

--- alsa/config_wm8962.h
+++ alsa/config_wm8962.h
@@ -231,8 +231,10 @@ static struct audio_card  wm8962_card = {
     .card                = 0,
     .out_rate            = 0,
     .out_channels        = 0,
+    .out_format          = 0,
     .in_rate             = 0,
     .in_channels         = 0,
+    .in_format           = 0,
 };
 
 #endif  /* ANDROID_INCLUDE_IMX_CONFIG_WM8962_H */
