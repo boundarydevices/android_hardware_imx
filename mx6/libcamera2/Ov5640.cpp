b2ad0709e2de7385739ffc71b848c21f518ec4c1
Xiaowen Liu <b37945@freescale.com>
2013-01-09 12:56:51 +0800

    ENGR00215174 fix cts failure for camera hal v2.
    add frame request.
    refine RequestManager to reduce stop count.
    refine video snap shot code.
    
    Signed-off-by: Xiaowen Liu <b37945@freescale.com>

--- mx6/libcamera2/Ov5640.cpp
+++ mx6/libcamera2/Ov5640.cpp
@@ -54,8 +54,7 @@ status_t Ov5640::initSensorInfo()
     sensorFormats[index++] = v4l2_fourcc('B', 'L', 'O', 'B');
     sensorFormats[index++] = v4l2_fourcc('R', 'A', 'W', 'S');
     //mAvailableFormats[2] = v4l2_fourcc('Y', 'U', 'Y', 'V');
-    index = 4;
-
+    mAvailableFormatCount = index;
     changeSensorFormats(sensorFormats, index);
 
     index = 0;
@@ -108,6 +107,9 @@ status_t Ov5640::initSensorInfo()
         }
     } // end while
 
+    mPreviewResolutionCount = previewCnt;
+    mPictureResolutionCount = pictureCnt;
+
     mMinFrameDuration = 33331760L;
     mMaxFrameDuration = 30000000000L;
     int i;
@@ -129,6 +131,7 @@ status_t Ov5640::initSensorInfo()
 
     setMaxPictureResolutions();
     FLOGI("mMaxWidth:%d, mMaxHeight:%d", mMaxWidth, mMaxHeight);
+    mFocalLength = 10.001;
 
     return NO_ERROR;
 }
