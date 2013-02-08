ac394739b8d42d42c8a00ccbba468675e754e19c
Nitin Garg <nitin.garg@freescale.com>
2013-02-08 13:44:35 -0600

    ENGR00244231: BT Add Atheros ath3k Bluetooth HAL for JB4.2
    Add Atheros ath3k Bluetooth HAL for JB4.2 bluedroid stack.
    
    Signed-off-by: Nitin Garg <nitin.garg@freescale.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-common_imx_dirs := libsensors libgps lights wlan
+common_imx_dirs := libsensors libgps lights wlan libbt-ath3k
 mx5x_dirs := $(common_imx_dirs) mx5x/audio mx5x/libcopybit mx5x/libgralloc  mx5x/hwcomposer mx5x/libcamera
 mx6_dirs := $(common_imx_dirs) alsa mx6/libgralloc_wrapper mx6/hwcomposer mx6/power
 
