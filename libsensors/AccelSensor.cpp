edb5151f35600afebc2c42e8302e15d22715d245
b38342 <b38342@freescale.com>
2014-01-02 23:43:10 +0800

    ENGR00294178 HAL:update sensor HAL to support batch interface
    Batch feature is a new feature in KK4.4. Update HAL codes to support
    it. Patch is from sensor team. Just tune code style and cherry pick
    it here.
    
    Signed-off-by: b38342 <b38342@freescale.com>
    Signed-off-by: Jianzheng Zhou <jianzheng.zhou@freescale.com>

--- libsensors/AccelSensor.cpp
+++ libsensors/AccelSensor.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2013 Freescale Semiconductor, Inc.
+ * Copyright (C) 2012-2014 Freescale Semiconductor, Inc.
  * Copyright (C) 2008 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,21 +28,28 @@
 #include <cutils/properties.h>
 #include "AccelSensor.h"
 #define ACC_DATA_NAME    "FreescaleAccelerometer" 
+#define ACC_FIFO_NAME    "/dev/mma8x5x"
 #define ACC_SYSFS_PATH   "/sys/class/input"
-#define ACC_SYSFS_DELAY  "poll"
+#define ACC_SYSFS_DELAY  "poll_delay"
 #define ACC_SYSFS_ENABLE "enable"
+#define ACC_SYSFS_FIFO	 "fifo"
+
 #define ACC_EVENT_X ABS_X
 #define ACC_EVENT_Y ABS_Y
 #define ACC_EVENT_Z ABS_Z
-#define ACC_DATA_CONVERSION(value) (float)((float)((int)value) * (GRAVITY_EARTH / (0x4000)))
+#define ACC_DATA_CONVERSION(value) (float)((float)((int)(value)) * (GRAVITY_EARTH / (0x4000)))
+#define ACC_FIFO_MAX_TIMEOUT	20480000000LL		//(640 * ACC_FIFO_LEN *1000000) ns
+#define ACC_FIFO_MIN_TIMEOUT	   40000000LL		//(1.25 * ACC_FIFO_LEN *1000000)     ns
 
 AccelSensor::AccelSensor()
-: SensorBase(NULL, ACC_DATA_NAME),
+: SensorBase(NULL, ACC_DATA_NAME,ACC_FIFO_NAME),
       mEnabled(0),
       mPendingMask(0),
       mInputReader(4),
       mDelay(0)
 {
+	mBatchEnabled = 0;
+	mUser = 0;
     memset(&mPendingEvent, 0, sizeof(mPendingEvent));
 	memset(mClassPath, '\0', sizeof(mClassPath));
 	
@@ -50,7 +57,6 @@ AccelSensor::AccelSensor()
     mPendingEvent.sensor  = ID_A;
     mPendingEvent.type    = SENSOR_TYPE_ACCELEROMETER;
     mPendingEvent.acceleration.status = SENSOR_STATUS_ACCURACY_HIGH;
-	mUser = 0;
 	if(sensor_get_class_path(mClassPath))
 	{
 		ALOGE("Can`t find the Acc sensor!");
@@ -67,28 +73,28 @@ int AccelSensor::setEnable(int32_t handle, int en)
     uint32_t newState  = en;
 	if(handle != ID_A && handle != ID_O && handle != ID_M)
 		return -1;
-	if(en)
+	/*mUser to control is sensor enable
+         *mEnable to control is Acc to report data
+	*/
+	if(en){
 		mUser++;
+		if(handle == ID_A)
+			mEnabled++;
+	}
 	else{
 		mUser--;
-		if(mUser < 0)
-			mUser = 0;
+		if(handle == ID_A)
+			mEnabled--;
 	}
-	if(mUser > 0)
+	if(mUser < 0)
+		mUser = 0;
+	if(mEnabled < 0)
+		mEnabled = 0;
+	if(mUser)
 		err = enable_sensor();
 	else
 		err = disable_sensor();
-	if(handle == ID_A ) {
-		if(en)
-         	mEnabled++;
-		else
-			mEnabled--;
-
-		if(mEnabled < 0)
-			mEnabled = 0;
-    }
-	update_delay();
-	ALOGD("AccelSensor enable %d ,usercount %d, handle %d ,mEnabled %d",en ,mUser, handle ,mEnabled);
+	ALOGD("AccelSensor enable %d , handle %d ,mEnabled %d",en , handle ,mEnabled);
     return err;
 }
 
@@ -100,50 +106,143 @@ int AccelSensor::setDelay(int32_t handle, int64_t ns)
     mDelay = ns;
     return update_delay();
 }
-
+bool AccelSensor::hasPendingEvents(){
+	return (mFifoCount > 0);
+}
 int AccelSensor::update_delay()
 {
     return set_delay(mDelay);
 }
-
+/*
+struct mma8x5x_data_axis {
+	short x;
+	short y;
+	short z;
+};
+struct mma8x5x_fifo{
+	int count;
+	int64_t period;
+	int64_t timestamp;
+	struct mma8x5x_data_axis fifo_data[MMA8X5X_FIFO_SIZE];
+};
+*/
+int AccelSensor::read_fifo(){
+	char buf[256];
+	int i = 0;
+	int nread,n;
+	int count;
+	int64_t period;
+	int64_t timestamp;
+	int offset = 0;
+	int16_t * data;
+	int16_t axis;
+	struct pollfd mPollFds[1];
+	if(fifo_fd){
+		mPollFds[0].fd = fifo_fd;
+		mPollFds[0].events = POLLIN;
+		mPollFds[0].revents = 0;
+		memset(buf,0,sizeof(buf));
+		n = poll(mPollFds, 1, 0);  //re-check if fifo is ready
+		if(n <= 0){
+			mFifoCount = 0;
+			return -EINVAL;
+		}
+		nread = read(fifo_fd, buf,sizeof(buf));
+		if(nread > 0){
+			mFifoCount = 0;
+			offset = 0;
+			count = *((int *)(&buf[offset]));
+			if(count > 0 && count <= MMA8X5X_FIFO_SIZE){
+				offset += sizeof(count);
+				period = *((int64_t *)(&buf[offset]));
+				offset += sizeof(period);
+				timestamp = *((int64_t *)(&buf[offset]));
+				offset += sizeof(timestamp);
+				data = ((int16_t *)(&buf[offset]));
+				for(i = 0 ; i <  count; i++){
+					axis = *data++;
+					mFifoPendingEvent[i].acceleration.x = ACC_DATA_CONVERSION(axis);
+					axis = *data++;
+					mFifoPendingEvent[i].acceleration.y = ACC_DATA_CONVERSION(axis);
+					axis = *data++;
+					mFifoPendingEvent[i].acceleration.z = ACC_DATA_CONVERSION(axis);
+					mFifoPendingEvent[i].timestamp = timestamp - (count - i -1)* period;
+					mFifoPendingEvent[i].sensor  = ID_A;
+					mFifoPendingEvent[i].type    = SENSOR_TYPE_ACCELEROMETER;
+					mFifoPendingEvent[i].acceleration.status = SENSOR_STATUS_ACCURACY_HIGH;
+				}
+				mFifoCount = count;
+			}
+		}
+	}
+	return 0;
+}
 int AccelSensor::readEvents(sensors_event_t* data, int count)
 {
+	int events = 0;
+	int ret;
     if (count < 1)
         return -EINVAL;
-
-    ssize_t n = mInputReader.fill(data_fd);
-    if (n < 0)
-        return n;
-
-    int numEventReceived = 0;
-    input_event const* event;
-    while (count && mInputReader.readEvent(&event)) {
-        int type = event->type;
-        if ((type == EV_ABS) || (type == EV_REL) || (type == EV_KEY)) {
-            processEvent(event->code, event->value);
-            mInputReader.next();
-        } else if (type == EV_SYN) {
-            int64_t time = timevalToNano(event->time);
-			if (mPendingMask) {
-				mPendingMask = 0;
-				mPendingEvent.timestamp = time;
-				if (mEnabled) {
-					*data++ = mPendingEvent;
-					count--;
-					numEventReceived++;
-				}
+	int numEventReceived = 0;
+	if(mBatchEnabled){
+		sensors_event_t event;
+		if(mBatchEnabled){
+			/*read the batch FIFO infomation,then add the META_DATA_VERSION event at the end of FIFO */
+			if(mFifoCount <= 0){
+				ret = read_fifo();
+				if(ret < 0)  /*not fifo data ,return immediately*/
+					return 0;
 			}
-            if (!mPendingMask) {
+			events = (count -1 < mFifoCount)? count -1 : mFifoCount;
+			if(events){
+				memcpy(data,&mFifoPendingEvent[MMA8X5X_FIFO_SIZE - events],sizeof(sensors_event_t) *events);
+				mFifoCount -= events;
+				numEventReceived += events;
+				data += events;
+			}
+			if(count > 0 && mFifoCount == 0){  //end fifo flag
+				memset(&event,0,sizeof(event));
+				event.version = META_DATA_VERSION;
+				event.type = SENSOR_TYPE_META_DATA;
+				event.meta_data.sensor = ID_A;
+				event.meta_data.what = 0;
+				*data++ = event;
+				count--;
+				numEventReceived++;
+			}
+		}
+	}else{
+		ssize_t n = mInputReader.fill(data_fd);
+		if (n < 0)
+			return n;
+		input_event const* event;
+        while (count && mInputReader.readEvent(&event)) {
+            int type = event->type;
+            if ((type == EV_ABS) || (type == EV_REL) || (type == EV_KEY)) {
+                processEvent(event->code, event->value);
                 mInputReader.next();
-            }
-        } else {
-            ALOGE("AccelSensor: unknown event (type=%d, code=%d)",
-                    type, event->code);
-            mInputReader.next();
-        }
-    }
-
-    return numEventReceived;
+            } else if (type == EV_SYN) {
+                int64_t time = timevalToNano(event->time);
+                if (mPendingMask) {
+                    mPendingMask = 0;
+                    mPendingEvent.timestamp = time;
+                    if (mEnabled) {
+                        *data++ = mPendingEvent;
+                        count--;
+                        numEventReceived++;
+                    }
+                }
+                if (!mPendingMask) {
+                    mInputReader.next();
+	            }
+	        } else {
+	            ALOGE("AccelSensor: unknown event (type=%d, code=%d)",
+	                    type, event->code);
+	            mInputReader.next();
+	        }
+	    }
+	}
+	return numEventReceived;
 }
 
 void AccelSensor::processEvent(int code, int value)
@@ -298,6 +397,56 @@ int AccelSensor::sensor_get_class_path(char *class_path)
 		*class_path = '\0';
 		return -1;
 	}
+}
+int AccelSensor::fifo(int64_t period_ns,int64_t timeout_ns,int wakeup){
+	char attr[PATH_MAX] = {'\0'};
+	char buf[256];
+	int timeout_ms = timeout_ns /1000000;
+	int period_ms = period_ns /1000000;
+	if(mClassPath[0] == '\0')
+		return -1;
+	strcpy(attr, mClassPath);
+	strcat(attr,"/");
+	strcat(attr,ACC_SYSFS_FIFO);
+	int fd = open(attr, O_RDWR);
+	if (0 > fd) {
+		ALOGE("Could not open (write-only) SysFs attribute \"%s\" (%s).", attr, strerror(errno));
+		return -errno;
+	}
+	sprintf(buf,"%d,%d,%d",period_ms,timeout_ms,(wakeup > 0 ? 1 : 0));
+	int err = 0;
+	err = write(fd,buf,strlen(buf) + 1);
+	if (0 > err) {
+		err = -errno;
+		ALOGE("Could not write SysFs attribute \"%s\" (%s).", attr, strerror(errno));
+	} else {
+		err = 0;
+	}
+	close(fd);
+	return 0;
+}
+int AccelSensor::batch(int handle, int flags, int64_t period_ns, int64_t timeout){
+	int wakeup;
+	if(flags & SENSORS_BATCH_DRY_RUN){
+		if(timeout > ACC_FIFO_MAX_TIMEOUT)
+			return -EINVAL;
+		else
+			return 0;
+	}else{
+		wakeup = 0;
+		if(flags & SENSORS_BATCH_WAKE_UPON_FIFO_FULL)
+			wakeup = 1;
+		fifo(period_ns,timeout,wakeup);
+		mDelay = period_ns;
+		if(timeout > 0)
+			mBatchEnabled = true;
+		else
+			mBatchEnabled = false;
+	}
+	return 0;
+}
+int AccelSensor::flush(int handle){
+	return -1;
 }
 
 /*****************************************************************************/
