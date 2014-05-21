ad2f45d429a06a3ddb6aca43d0431aea3936ce34
guoyin.chen <guoyin.chen@freescale.com>
2014-05-21 13:32:18 +0800

    ENGR00314547 Mediaserver crash when no camera been connected
    Crashed at ~DeviceAdapter() for free an uninitialised mVideoInfo
    Fixed by init the mVideoInfo to NULL
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- mx6/libcamera2/DeviceAdapter.cpp
+++ mx6/libcamera2/DeviceAdapter.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2013 Freescale Semiconductor, Inc.
+ * Copyright (C) 2012-2014 Freescale Semiconductor, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,7 +53,7 @@ sp<DeviceAdapter>DeviceAdapter::Create(const CameraInfo& info)
 }
 
 DeviceAdapter::DeviceAdapter()
-    : mCameraHandle(-1), mQueued(0), mCpuNum(0)
+    : mVideoInfo(NULL), mCameraHandle(-1), mQueued(0), mCpuNum(0)
 {}
 
 DeviceAdapter::~DeviceAdapter()
