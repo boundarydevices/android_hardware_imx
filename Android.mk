38d9b7686dec6655ae28b375768d838c918c5e5b
guoyin.chen <guoyin.chen@freescale.com>
2015-04-20 20:29:27 +0800

    MA-6560-1 No hardware hal library for imx7 soc
    Add HAL directory for imx7 soc type
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,7 @@
 common_imx_dirs := libsensors libgps lights wlan libbt-ath3k
 mx5x_dirs := $(common_imx_dirs) mx5x/audio mx5x/libcopybit mx5x/libgralloc  mx5x/hwcomposer mx5x/libcamera mx5x/power
 mx6_dirs := $(common_imx_dirs) alsa mx6/libgralloc_wrapper mx6/hwcomposer mx6/power mx6/consumerir
+mx7_dirs := $(common_imx_dirs) alsa mx6/power
 
 ifeq ($(TARGET_BOARD_PLATFORM),imx6)
   ifeq ($(IMX_CAMERA_HAL_V2),true)
@@ -10,7 +11,16 @@ ifeq ($(TARGET_BOARD_PLATFORM),imx6)
   endif
   include $(call all-named-subdir-makefiles,$(mx6_dirs))
 else
-  ifeq ($(TARGET_BOARD_PLATFORM),imx5x)
-    include $(call all-named-subdir-makefiles,$(mx5x_dirs))
+  ifeq ($(TARGET_BOARD_PLATFORM),imx7)
+    ifeq ($(IMX_CAMERA_HAL_V2),true)
+      mx7_dirs += mx6/libcamera2
+    else
+      mx7_dirs += mx6/libcamera
+    endif
+    include $(call all-named-subdir-makefiles,$(mx7_dirs))
+  else
+    ifeq ($(TARGET_BOARD_PLATFORM),imx5x)
+      include $(call all-named-subdir-makefiles,$(mx5x_dirs))
+    endif
   endif
 endif
