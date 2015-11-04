1d1be3ac09afa8b6102c0b6ca46eddf7bf27f81d
guoyin.chen <guoyin.chen@freescale.com>
2015-11-04 13:07:58 +0800

    MA-7210 Add missed HAL folder
    Add libsensors_sensorhub mx6/libcamera and mx6/libcamera2
    Remove mx6/consumerir in HAL folder
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,7 @@
 imx_dirs := libsensors libgps lights wlan libbt-ath3k \
-            alsa mx6/libgralloc_wrapper mx6/hwcomposer \
-            mx6/power mx6/consumerir mx7/gralloc mx7/hwcomposer \
+            alsa libsensors_sensorhub mx6/libgralloc_wrapper \
+            mx6/hwcomposer mx6/power mx6/libcamera mx6/libcamera2 \
+            mx7/gralloc mx7/hwcomposer \
 
 include $(call all-named-subdir-makefiles,$(imx_dirs))
 
