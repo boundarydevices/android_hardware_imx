a899525276880babbcc9c636445f4689c7ac5ce0
zhang sanshan <b51434@freescale.com>
2016-05-25 12:17:38 +0800

    MA-7974 Add FSL lisence
    Add fsl lisence into switchprofile.cpp.
    
    Change-Id: I1345e1a332b4aa4957d623deac0dd0d32b6dd3b3
    Signed-off-by: zhang sanshan <b51434@freescale.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/199
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- mx6/power/switchprofile.cpp
+++ mx6/power/switchprofile.cpp
@@ -1,6 +1,4 @@
 /*
- * Copyright (C) 2012 Freescale Semiconductor, Inc. All Rights Reserved.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,6 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* Copyright (C) 2012,2016 Freescale Semiconductor, Inc. */
 
 #include "switchprofile.h"
 #define LOG_TAG "SWITCHPROFILE"
