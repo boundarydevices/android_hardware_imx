f0f8a8568f24f717c5977f2845b0cecc21b94ee6
Richard Liu <xuegang.liu@nxp.com>
2016-06-30 06:45:22 +0800

    MA-7879-1 convert LOCAL_MODULE_PATH to LOCAL_MODULE_RELATIVE_PATH for mx8dv
    LOCAL_MODULE_PATH doesn't work for multiarch builds, replace it
    with LOCAL_MODULE_RELATIVE_PATH.
    
    Change-Id: Iea6d4e7759fe8cbb1e95ca84b1cfbc9d25b494c8
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/575
    Reviewed-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- mx6/power/Android.mk
+++ mx6/power/Android.mk
@@ -20,7 +20,7 @@ LOCAL_PATH := $(call my-dir)
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
 LOCAL_SRC_FILES := power.cpp switchprofile.cpp messageQueue.cpp
 LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
