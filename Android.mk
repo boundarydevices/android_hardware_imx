18787b6bf5cb2b52da77866a20a25a64c1465f26
zhang sanshan <b51434@freescale.com>
2016-08-08 17:16:21 +0800

    MA-8532 [Android_IMX8]HAL: remove imx HAL android.mk
    Android.mk is not necessary in imx HAL.
    
    Change-Id: I255ed5dc9c54653ca0165f9b378591a73c39fe01
    Signed-off-by: zhang sanshan <b51434@freescale.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/767
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    (cherry picked from commit a8415d3f4c47b102792d3b6361e6104f38b47dad)
    Reviewed-on: http://androidsource.ap.freescale.net/project/768

--- Android.mk
+++
@@ -1,7 +0,0 @@
-imx_dirs := libsensors libgps lights wlan libbt-ath3k \
-            alsa libsensors_sensorhub mx6/libgralloc_wrapper \
-            mx6/hwcomposer mx6/power mx6/libcamera mx6/libcamera3 \
-            mx7/gralloc mx7/hwcomposer \
-
-include $(call all-named-subdir-makefiles,$(imx_dirs))
-
