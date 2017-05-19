d62ee22f69901229e33ccd46fb1aa30d71f24691
fang hui <hui.fang@nxp.com>
2017-05-19 14:00:54 +0800

    MA-9617 [audioHAL] license issue in pcm_ext.c
    Copy the whole declaration of AOSP license, not just the head.
    
    Change-Id: Iebdbaaab2742478622147245d933a061740382a8
    Signed-off-by: fang hui <hui.fang@nxp.com>

--- alsa/pcm_ext.c
+++ alsa/pcm_ext.c
@@ -1,6 +1,30 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright 2011, The Android Open Source Project
+ * Copyright 2017 NXP
  *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of The Android Open Source Project nor the names of
+ *       its contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY The Android Open Source Project ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL The Android Open Source Project BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,7 +37,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* Copyright 2017 NXP */
 
 #include <unistd.h>
 #include <sys/types.h>
