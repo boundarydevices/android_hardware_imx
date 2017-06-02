530f7691d5c34c1b37ee0b92283820253a8be4c4
ivan.liu <xiaowen.liu@nxp.com>
2017-06-02 18:32:24 +0800

    MA-9688 Fix Blob format issue on Gralloc_v3.
    GPU can't recognize BLOB format, fake format to YUYV.
    BLOB format buffer size = width*height, and height = 1;
    YUYV format buffer size = width * height * 2;
    and width or height may be aligned to some value.
    set height to height*16 to avoid alignment issue.
    
    Change-Id: Idfd0b06d64a8ab27f0f69f8ddf63fb4c26efb4c2
    Signed-off-by: ivan.liu <xiaowen.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/2057
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-by: juan.hao <juan.hao@nxp.com>

--- display/gralloc_v3/gralloc.cpp
+++ display/gralloc_v3/gralloc.cpp
@@ -344,6 +344,14 @@ static int gralloc_allocate(gralloc1_device_t* device,
         }
 
         desc->mFslFormat = convertAndroidFormat(desc->mFormat);
+        if (desc->mFslFormat == FORMAT_BLOB) {
+            // GPU can't recognize BLOB format, fake format to YUYV.
+            // size = width * height * 2;
+            // avoid height alignment issue.
+            desc->mFormat = HAL_PIXEL_FORMAT_YCbCr_422_I;
+            desc->mWidth = desc->mWidth / 32;
+            desc->mHeight = desc->mHeight * 16;
+        }
         desc->checkFormat();
 
         int ret = pManager->allocMemory(*desc, &memory);
