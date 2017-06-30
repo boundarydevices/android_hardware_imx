727fc347f5ce3221505b813a00ff6b7a1f46e126
ivan.liu <xiaowen.liu@nxp.com>
2017-06-30 17:42:24 +0800

    MA-9813 change ion_phys implementation based on ION_IOC_PHYS_DMA.
    ION_IOC_PHYS can't be used on ARM64 platform.
    Change to ION_IOC_PHYS_DMA to get ion phys.
    
    Change-Id: I6899916549f8746ed072e2764e91233f4ba811e9
    Signed-off-by: ivan.liu <xiaowen.liu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/2295
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- include/ion_ext.h
+++ include/ion_ext.h
@@ -5,13 +5,13 @@
 #ifndef _ION_EXT_H
 #define _ION_EXT_H
 
-static inline unsigned long ion_phys(int fd, ion_user_handle_t handle) {
-    struct ion_phys_data local_ion_phys_data = {
-        .handle = handle, .phys = 0,
+static inline unsigned long ion_phys(int fd, size_t size, int dmafd) {
+    struct ion_phys_dma_data local_ion_phys_data = {
+        .phys = 0, .size = size, .dmafd = dmafd,
     };
 
     struct ion_custom_data local_ion_custom_data = {
-        .cmd = ION_IOC_PHYS, .arg = (unsigned long)&local_ion_phys_data,
+        .cmd = ION_IOC_PHYS_DMA, .arg = (unsigned long)&local_ion_phys_data,
     };
 
     return ioctl(fd, ION_IOC_CUSTOM, &local_ion_custom_data) ? 0 : local_ion_phys_data.phys;
