a3bed3215ebedea9a3ab01657e843fc6610777a8
Gary Bisson <gary.bisson@boundarydevices.com>
2018-03-15 13:25:22 +0100

    lights: add LIGHT_ID_BATTERY basic support
    FLASH_TIMED type of light isn't supported in the kernel right now.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- lights/fsl_lights.c
+++ lights/fsl_lights.c
@@ -56,10 +56,79 @@ struct lights_module_t HAL_MODULE_INFO_SYM = {
     }
 };
 
-static char max_path[256], path[256];
+static char backlight_max_path[256], backlight_path[256];
+
+#if BOARD_HAS_BATTERY_LIGHTS
+char const*const RED_LED_FILE
+        = "/sys/class/leds/red/brightness";
+
+char const*const GREEN_LED_FILE
+        = "/sys/class/leds/green/brightness";
+
+char const*const BLUE_LED_FILE
+        = "/sys/class/leds/blue/brightness";
+
+static int rgb_brightness_ratio = 255;
+#endif
+
 // ****************************************************************************
 // module
 // ****************************************************************************
+static int
+write_int(char const* path, int value)
+{
+    int fd;
+    static int already_warned = 0;
+
+    fd = open(path, O_WRONLY);
+    if (fd >= 0) {
+        char buffer[20];
+        size_t bytes = snprintf(buffer, sizeof(buffer), "%d\n", value);
+        if(bytes >= sizeof(buffer)) return -EINVAL;
+        ssize_t amt = write(fd, buffer, bytes);
+        close(fd);
+        return amt == -1 ? -errno : 0;
+    } else {
+        if (already_warned == 0) {
+            ALOGE("write_int failed to open %s\n", path);
+            already_warned = 1;
+        }
+        return -errno;
+    }
+}
+
+#if BOARD_HAS_BATTERY_LIGHTS
+static int set_light_battery(struct light_device_t* dev,
+                             struct light_state_t const* state)
+{
+    int red, green, blue;
+    unsigned int colorRGB;
+
+    if (!dev)
+        return -1;
+
+    if (state->flashMode == LIGHT_FLASH_TIMED) {
+        ALOGI("Discard FLASH_TIMED info as not supported!");
+        return 0;
+    }
+
+    colorRGB = state->color;
+
+    ALOGV("set_speaker_light_locked mode %d, colorRGB=%08X\n",
+            state->flashMode, colorRGB);
+
+    red = ((colorRGB >> 16) & 0xFF) * rgb_brightness_ratio / 255;
+    green = ((colorRGB >> 8) & 0xFF) * rgb_brightness_ratio / 255;
+    blue = (colorRGB & 0xFF) * rgb_brightness_ratio / 255;
+
+    write_int(RED_LED_FILE, red);
+    write_int(GREEN_LED_FILE, green);
+    write_int(BLUE_LED_FILE, blue);
+
+    return 0;
+}
+#endif
+
 static int set_light_backlight(struct light_device_t* dev,
                                struct light_state_t const* state)
 {
@@ -69,13 +138,16 @@ static int set_light_backlight(struct light_device_t* dev,
     unsigned int i = 0;
     FILE *file;
 
+    if (!dev)
+        return -1;
+
     brightness = ((77*((color>>16)&0x00ff)) + (150*((color>>8)&0x00ff)) +
                  (29*(color&0x00ff))) >> 8;
     ALOGV("set_light, get brightness=%d", brightness);
 
-    file = fopen(max_path, "r");
+    file = fopen(backlight_max_path, "r");
     if (!file) {
-        ALOGE("can not open file %s\n", max_path);
+        ALOGE("can not open file %s\n", backlight_max_path);
         return result;
     }
     fread(&max_brightness, 1, 3, file);
@@ -97,19 +169,12 @@ static int set_light_backlight(struct light_device_t* dev,
     ALOGV("set_light, max_brightness=%d, target brightness=%d",
         max_brightness, brightness);
 
-    file = fopen(path, "w");
-    if (!file) {
-        ALOGE("can not open file %s\n", path);
-        return result;
-    }
-    fprintf(file, "%d", brightness);
-    fclose(file);
+    result = write_int(backlight_path, brightness);
 
-    result = 0;
     return result;
 }
 
-static int light_close_backlight(struct hw_device_t *dev)
+static int close_lights(struct hw_device_t *dev)
 {
     struct light_device_t *device = (struct light_device_t*)dev;
     if (device)
@@ -121,29 +186,16 @@ static int light_close_backlight(struct hw_device_t *dev)
 static int lights_device_open(const struct hw_module_t* module,
                               const char* name, struct hw_device_t** device)
 {
-    int status = -EINVAL;
-    ALOGV("lights_device_open\n");
+    int (*set_light)(struct light_device_t* dev,
+            struct light_state_t const* state);
+
     if (!strcmp(name, LIGHT_ID_BACKLIGHT)) {
-        struct light_device_t *dev;
         int fdfb0;
         char fbtype[256];
         struct dirent **namelist;
         int n, idx = 0;
 
-        dev = malloc(sizeof(*dev));
-
-        /* initialize our state here */
-        memset(dev, 0, sizeof(*dev));
-
-        /* initialize the procs */
-        dev->common.tag = HARDWARE_DEVICE_TAG;
-        dev->common.version = 0;
-        dev->common.module = (struct hw_module_t*) module;
-        dev->common.close = light_close_backlight;
-
-        dev->set_light = set_light_backlight;
-
-        *device = &dev->common;
+        set_light = set_light_backlight;
 
         strcpy(fbtype,"ldb"); /* default */
         fdfb0 = open("/sys/class/graphics/fb0/fsl_disp_dev_property", O_RDONLY);
@@ -174,15 +226,15 @@ static int lights_device_open(const struct hw_module_t* module,
         n = scandir(DEF_BACKLIGHT_PATH, &namelist, NULL, alphasort);
         if (n < 0) {
             ALOGE("couldn't scandir %s", DEF_BACKLIGHT_PATH);
-            return status;
+            return -EINVAL;
         }
 
         /* Use first match (alphabetic order) */
         while (idx++ < n) {
             if (strstr(namelist[idx]->d_name, fbtype) != NULL) {
-                strcpy(path, DEF_BACKLIGHT_PATH);
-                strcat(path, namelist[idx]->d_name);
-                ALOGI("found backlight under %s", path);
+                strcpy(backlight_path, DEF_BACKLIGHT_PATH);
+                strcat(backlight_path, namelist[idx]->d_name);
+                ALOGI("found backlight under %s", backlight_path);
                 break;
             }
         }
@@ -192,30 +244,38 @@ static int lights_device_open(const struct hw_module_t* module,
             free(namelist[n]);
         free(namelist);
 
-        /* Check if a path has been found */
-        if (strstr(path, DEF_BACKLIGHT_PATH) == NULL) {
+        /* Check if a backlight_path has been found */
+        if (strstr(backlight_path, DEF_BACKLIGHT_PATH) == NULL) {
             ALOGE("didn't find any matching backlight");
-            return status;
+            return -EINVAL;
         }
 
-        strcpy(max_path, path);
-        strcat(max_path, "/max_brightness");
-        strcat(path, "/brightness");
+        strcpy(backlight_max_path, backlight_path);
+        strcat(backlight_max_path, "/max_brightness");
+        strcat(backlight_path, "/brightness");
 
-        file = fopen(max_path, "r");
-        if (!file) {
-            free(dev);
-            ALOGE("cannot open backlight file %s\n", max_path);
-            return status;
-        }
-        fclose(file);
-
-        ALOGI("max backlight file is %s\n", max_path);
-        ALOGI("backlight brightness file is %s\n", path);
-
-        status = 0;
+        ALOGI("max backlight file is %s\n", backlight_max_path);
+        ALOGI("backlight brightness file is %s\n", backlight_path);
     }
+#if BOARD_HAS_BATTERY_LIGHTS
+    else if (0 == strcmp(LIGHT_ID_BATTERY, name))
+        set_light = set_light_battery;
+#endif
 
-    /* todo other lights device init */
-    return status;
+    struct light_device_t *dev = malloc(sizeof(struct light_device_t));
+
+    if(!dev)
+        return -ENOMEM;
+
+    memset(dev, 0, sizeof(*dev));
+
+    dev->common.tag = HARDWARE_DEVICE_TAG;
+    dev->common.version = 0;
+    dev->common.module = (struct hw_module_t*) module;
+    dev->common.close = close_lights;
+    dev->set_light = set_light;
+
+    *device = (struct hw_device_t*)dev;
+
+    return 0;
 }
