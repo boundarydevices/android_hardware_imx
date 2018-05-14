24c5a806ccde1cd5ee1198f09ff87626999f537e
Gary Bisson <gary.bisson@boundarydevices.com>
2018-05-14 12:43:57 +0200

    libcamera3: allow overriding flash and torch brightness
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- libcamera3/Camera.cpp
+++ libcamera3/Camera.cpp
@@ -999,7 +999,7 @@ int32_t Camera::getV4l2Res(uint32_t streamWidth, uint32_t streamHeight, uint32_t
 uint8_t Camera::setFlashlight(uint8_t mode)
 {
     FILE *file;
-    unsigned int brightness = 0;
+    int32_t brightness = -1;
     bool on = false;
     char max_path[PATH_LEN], path[PATH_LEN];
 
@@ -1011,6 +1011,7 @@ uint8_t Camera::setFlashlight(uint8_t mode)
         case ANDROID_FLASH_MODE_SINGLE:
             snprintf(max_path, PATH_LEN, "%s/max_brightness", FLASH_PATH);
             snprintf(path, PATH_LEN, "%s/brightness", FLASH_PATH);
+            brightness = property_get_int32("persist.camera.flash.brightness", -1);
             on = true;
             break;
         case ANDROID_FLASH_MODE_TORCH:
@@ -1019,10 +1020,11 @@ uint8_t Camera::setFlashlight(uint8_t mode)
         default:
             snprintf(max_path, PATH_LEN, "%s/max_brightness", TORCH_PATH);
             snprintf(path, PATH_LEN, "%s/brightness", TORCH_PATH);
+            brightness = property_get_int32("persist.camera.torch.brightness", -1);
             break;
     };
 
-    if (on) {
+    if (on && (brightness < 0)) {
         /* Read maximum value */
         file = fopen(max_path, "r");
         if (!file) {
