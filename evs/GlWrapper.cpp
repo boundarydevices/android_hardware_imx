cc33f41441b40dd686fc92d237e994177843e994
zhang sanshan <sanshan.zhang@nxp.com>
2018-08-04 21:20:00 +0800

    MA-12273 [Android] fix evs application issue
    add glTexParameteri in updateImageTexture.
    otherwise evs will have no display.
    
    Change-Id: I92b73e7514fe170e5a3a431ceaaffaf194392593
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/5249
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>

--- evs/GlWrapper.cpp
+++ evs/GlWrapper.cpp
@@ -393,13 +393,17 @@ bool GlWrapper::updateImageTexture(const BufferDesc& buffer) {
         if (mKHRimage == EGL_NO_IMAGE_KHR) {
             ALOGE("error creating EGLImage: %s", getEGLError());
             return false;
+        } else {
+            // Update the texture handle we already created to refer to this gralloc buffer
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, mTextureMap);
+            glEGLImageTargetTexture2DOES(GL_TEXTURE_2D, static_cast<GLeglImageOES>(mKHRimage));
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         }
-
-
-        // Update the texture handle we already created to refer to this gralloc buffer
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, mTextureMap);
-        glEGLImageTargetTexture2DOES(GL_TEXTURE_2D, static_cast<GLeglImageOES>(mKHRimage));
 
     }
 
