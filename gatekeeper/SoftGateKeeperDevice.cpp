a6e8772d2d859465eae5257aac432a5008ead1d2
Jindong <jindong.yue@nxp.com>
2019-03-11 10:04:55 +0800

    MA-14297 Modify Copyright format as: Copyright <year> NXP
    Change-Id: I8d327813fdba5bc6072e7cc2fbbfd09a117e005f
    Signed-off-by: Jindong <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/7651
    Reviewed-by: juan.hao <juan.hao@nxp.com>

--- gatekeeper/SoftGateKeeperDevice.cpp
+++ gatekeeper/SoftGateKeeperDevice.cpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The Android Open Source Project
- * Copyright NXP 2018
+ * Copyright 2018 NXP
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
