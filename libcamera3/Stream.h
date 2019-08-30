8a58555319d5487cc3036a663a15c596e0f8ec18
Laura Nao <laura.nao@kynetics.com>
2019-08-30 12:38:43 +0000

    Fix jpeg transport header offset
    As GPU can't recognize BLOB, the format is faked to YUYV. The size of
    the allocated graphic buffer is slightly larger than jpeg buffer size
    because of memory alignment. This results in jpeg transport header being
    written to a position different from the one expected in
    Image_getJpegSize() in android_media_Utils.cpp, causing the camera
    picture size to be always set to the maximum value.
    
    Signed-off-by: Laura Nao <laura.nao@kynetics.com>
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- libcamera3/Stream.h
+++ libcamera3/Stream.h
@@ -68,6 +68,7 @@ public:
     void dump(int fd);
 
 protected:
+    int32_t getJpegBufferSize(StreamBuffer& src, sp<Metadata> meta);
     int32_t processJpegBuffer(StreamBuffer& src, sp<Metadata> meta);
     int32_t processFrameBuffer(StreamBuffer& src, sp<Metadata> meta);
 
