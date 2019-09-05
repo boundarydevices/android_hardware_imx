3ae4dcf7cb62a8a78ce57e43f81032b747a8a745
zhang sanshan <sanshan.zhang@nxp.com>
2019-09-05 09:22:04 +0800

    MA-15399-1 update vehicle hal according andorid10.0
    copy from /hardware/interfaces/automotive/vehicle/2.0/default/
    
    Change-Id: I029c07495938bf2c3ac9b86967918bde6fb63140
    Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/9968
    Reviewed-by: Jindong Yue <jindong.yue@nxp.com>

--- vehicle/impl/vhal_v2_0/SocketComm.cpp
+++ vehicle/impl/vhal_v2_0/SocketComm.cpp
@@ -18,24 +18,15 @@
 
 #include <android/hardware/automotive/vehicle/2.0/IVehicle.h>
 #include <android/log.h>
+#include <arpa/inet.h>
 #include <log/log.h>
-#include <linux/netlink.h>
 #include <netinet/in.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 
 #include "SocketComm.h"
 
 // Socket to use when communicating with Host PC
-typedef struct user_msg_info {
-        struct nlmsghdr hdr;
-        char  msg[1024];
-} user_socket_info;
-
-#define MAX_PLOAD 1024
-#define PROTOCOL_ID 30
-
-#define SYNC_COMMANDS "sync"
+static constexpr int DEBUG_SOCKET = 33452;
 
 namespace android {
 namespace hardware {
@@ -45,137 +36,182 @@ namespace V2_0 {
 
 namespace impl {
 
-SocketComm::SocketComm() {
-    // Initialize member vars
-    mCurSockFd = -1;
-    mExit      =  0;
-    mSockFd    = -1;
-}
-
+SocketComm::SocketComm(MessageProcessor* messageProcessor)
+    : mListenFd(-1), mMessageProcessor(messageProcessor) {}
 
 SocketComm::~SocketComm() {
-    stop();
 }
 
-int SocketComm::connect() {
-    sockaddr_in cliAddr;
-    socklen_t cliLen = sizeof(cliAddr);
-    int cSockFd = accept(mSockFd, reinterpret_cast<struct sockaddr*>(&cliAddr), &cliLen);
-
-    if (cSockFd >= 0) {
-        {
-            std::lock_guard<std::mutex> lock(mMutex);
-            mCurSockFd = cSockFd;
-        }
-        ALOGD("%s: Incoming connection received on socket %d", __FUNCTION__, cSockFd);
-    } else {
-        cSockFd = -1;
+void SocketComm::start() {
+    if (!listen()) {
+        return;
     }
 
-    return cSockFd;
-}
-
-int SocketComm::open() {
-    int retVal;
-    struct sockaddr_nl servAddr;
-
-    mSockFd = socket(AF_NETLINK, SOCK_RAW, PROTOCOL_ID);
-    if (mSockFd < 0) {
-        ALOGE("%s: socket() failed, mSockFd=%d, errno=%d", __FUNCTION__, mSockFd, errno);
-        mSockFd = -1;
-        return -errno;
-    }
-
-    memset(&servAddr, 0, sizeof(servAddr));
-    servAddr.nl_family = AF_NETLINK;
-    servAddr.nl_pid = getpid();
-    servAddr.nl_groups = 0;
-
-    retVal = bind(mSockFd, (struct sockaddr *)&servAddr, sizeof(servAddr));
-    if(retVal < 0) {
-        ALOGE("%s: Error on binding: retVal=%d, errno=%d", __FUNCTION__, retVal, errno);
-        close(mSockFd);
-        return -errno;
-    }
-
-    std::vector<uint8_t> msg = std::vector<uint8_t>(sizeof(SYNC_COMMANDS));
-    memcpy(msg.data(), SYNC_COMMANDS, sizeof(SYNC_COMMANDS));
-    write(msg);
-
-    return 0;
-}
-
-std::vector<uint8_t> SocketComm::read() {
-    int ret;
-    user_socket_info u_info;
-    struct sockaddr_nl nl_socket;
-    socklen_t nl_socket_len = sizeof(nl_socket);
-    int message_size;
-
-    memset(&nl_socket, 0, sizeof(nl_socket));
-    memset(&u_info, 0, sizeof(user_socket_info));
-    nl_socket.nl_family = AF_NETLINK;
-    nl_socket.nl_pid = 0;
-    nl_socket.nl_groups = 0;
-
-    ret = recvfrom(mSockFd, &u_info, sizeof(u_info), 0, (struct sockaddr *)&nl_socket, &nl_socket_len);
-
-    message_size = u_info.hdr.nlmsg_len - sizeof(struct nlmsghdr);
-    if(ret < 0) {
-        ALOGE("recv message failed \n");
-        return std::vector<uint8_t>();
-    } else {
-        std::vector<uint8_t> msg = std::vector<uint8_t> (message_size);
-        memcpy(msg.data(), u_info.msg, message_size);
-        return msg;
-    }
+    mListenThread = std::make_unique<std::thread>(std::bind(&SocketComm::listenThread, this));
 }
 
 void SocketComm::stop() {
-    if (mExit == 0) {
-        std::lock_guard<std::mutex> lock(mMutex);
-        mExit = 1;
+    if (mListenFd > 0) {
+        ::close(mListenFd);
+        if (mListenThread->joinable()) {
+            mListenThread->join();
+        }
+        mListenFd = -1;
+    }
+}
 
-        // Close emulator socket if it is open
-        if (mCurSockFd != -1) {
-            close(mCurSockFd);
-            mCurSockFd = -1;
+void SocketComm::sendMessage(emulator::EmulatorMessage const& msg) {
+    std::lock_guard<std::mutex> lock(mMutex);
+    for (std::unique_ptr<SocketConn> const& conn : mOpenConnections) {
+        conn->sendMessage(msg);
+    }
+}
+
+bool SocketComm::listen() {
+    int retVal;
+    struct sockaddr_in servAddr;
+
+    mListenFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (mListenFd < 0) {
+        ALOGE("%s: socket() failed, mSockFd=%d, errno=%d", __FUNCTION__, mListenFd, errno);
+        mListenFd = -1;
+        return false;
+    }
+
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = INADDR_ANY;
+    servAddr.sin_port = htons(DEBUG_SOCKET);
+
+    retVal = bind(mListenFd, reinterpret_cast<struct sockaddr*>(&servAddr), sizeof(servAddr));
+    if(retVal < 0) {
+        ALOGE("%s: Error on binding: retVal=%d, errno=%d", __FUNCTION__, retVal, errno);
+        close(mListenFd);
+        mListenFd = -1;
+        return false;
+    }
+
+    ALOGI("%s: Listening for connections on port %d", __FUNCTION__, DEBUG_SOCKET);
+    ::listen(mListenFd, 1);
+    return true;
+}
+
+SocketConn* SocketComm::accept() {
+    sockaddr_in cliAddr;
+    socklen_t cliLen = sizeof(cliAddr);
+    int sfd = ::accept(mListenFd, reinterpret_cast<struct sockaddr*>(&cliAddr), &cliLen);
+
+    if (sfd > 0) {
+        char addr[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &cliAddr.sin_addr, addr, INET_ADDRSTRLEN);
+
+        ALOGD("%s: Incoming connection received from %s:%d", __FUNCTION__, addr, cliAddr.sin_port);
+        return new SocketConn(mMessageProcessor, sfd);
+    }
+
+    return nullptr;
+}
+
+void SocketComm::listenThread() {
+    while (true) {
+        SocketConn* conn = accept();
+        if (conn == nullptr) {
+            return;
         }
 
-        if (mSockFd != -1) {
-            close(mSockFd);
-            mSockFd = -1;
+        conn->start();
+        {
+            std::lock_guard<std::mutex> lock(mMutex);
+            mOpenConnections.push_back(std::unique_ptr<SocketConn>(conn));
         }
     }
 }
 
-int SocketComm::write(const std::vector<uint8_t>& data) {
-    int ret;
-    struct sockaddr_nl daddr;
-    struct nlmsghdr *nlh = NULL;
-    memset(&daddr, 0, sizeof(daddr));
-    daddr.nl_family = AF_NETLINK;
-    // 0 means this message is to kernel
-    daddr.nl_pid = 0;
-    daddr.nl_groups = 0;
+/**
+ * Called occasionally to clean up connections that have been closed.
+ */
+void SocketComm::removeClosedConnections() {
+    std::lock_guard<std::mutex> lock(mMutex);
+    std::remove_if(mOpenConnections.begin(), mOpenConnections.end(),
+                   [](std::unique_ptr<SocketConn> const& c) { return !c->isOpen(); });
+}
 
-    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PLOAD));
-    memset(nlh, 0, sizeof(struct nlmsghdr));
-    nlh->nlmsg_len = NLMSG_LENGTH(data.size());
-    nlh->nlmsg_flags = 0;
-    nlh->nlmsg_type = 0;
-    nlh->nlmsg_seq = 0;
-    nlh->nlmsg_pid = getpid();
-    memcpy(NLMSG_DATA(nlh), data.data(), data.size());
-    ret = sendto(mSockFd, nlh, nlh->nlmsg_len, 0, (struct sockaddr *)&daddr, sizeof(struct sockaddr_nl));
-    if(!ret) {
-        ALOGE("send message failed.\n");
+SocketConn::SocketConn(MessageProcessor* messageProcessor, int sfd)
+    : CommConn(messageProcessor), mSockFd(sfd) {}
+
+/**
+ * Reads, in a loop, exactly numBytes from the given fd. If the connection is closed, returns
+ * an empty buffer, otherwise will return exactly the given number of bytes.
+ */
+std::vector<uint8_t> readExactly(int fd, int numBytes) {
+    std::vector<uint8_t> buffer(numBytes);
+    int totalRead = 0;
+    int offset = 0;
+    while (totalRead < numBytes) {
+        int numRead = ::read(fd, &buffer.data()[offset], numBytes - offset);
+        if (numRead == 0) {
+            buffer.resize(0);
+            return buffer;
+        }
+
+        totalRead += numRead;
+    }
+    return buffer;
+}
+
+/**
+ * Reads an int, guaranteed to be non-zero, from the given fd. If the connection is closed, returns
+ * -1.
+ */
+int32_t readInt(int fd) {
+    std::vector<uint8_t> buffer = readExactly(fd, sizeof(int32_t));
+    if (buffer.size() == 0) {
         return -1;
     }
 
-    return 0;
+    int32_t value = *reinterpret_cast<int32_t*>(buffer.data());
+    return ntohl(value);
 }
 
+std::vector<uint8_t> SocketConn::read() {
+    int32_t msgSize = readInt(mSockFd);
+    if (msgSize <= 0) {
+        ALOGD("%s: Connection terminated on socket %d", __FUNCTION__, mSockFd);
+        return std::vector<uint8_t>();
+    }
+
+    return readExactly(mSockFd, msgSize);
+}
+
+void SocketConn::stop() {
+    if (mSockFd > 0) {
+        close(mSockFd);
+        mSockFd = -1;
+    }
+}
+
+int SocketConn::write(const std::vector<uint8_t>& data) {
+    static constexpr int MSG_HEADER_LEN = 4;
+    int retVal = 0;
+    union {
+        uint32_t msgLen;
+        uint8_t msgLenBytes[MSG_HEADER_LEN];
+    };
+
+    // Prepare header for the message
+    msgLen = static_cast<uint32_t>(data.size());
+    msgLen = htonl(msgLen);
+
+    if (mSockFd > 0) {
+        retVal = ::write(mSockFd, msgLenBytes, MSG_HEADER_LEN);
+
+        if (retVal == MSG_HEADER_LEN) {
+            retVal = ::write(mSockFd, data.data(), data.size());
+        }
+    }
+
+    return retVal;
+}
 
 }  // impl
 
