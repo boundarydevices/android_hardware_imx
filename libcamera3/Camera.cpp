4ac8da111408436dc57839f7f70e9a7d61081369
Gary Bisson <gary.bisson@boundarydevices.com>
2020-04-28 16:17:34 +0200

    libcamera3: ImxCamera: add full autofocus support
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- libcamera3/Camera.cpp
+++ libcamera3/Camera.cpp
@@ -73,8 +73,8 @@ Camera* Camera::createCamera(int32_t id,
     if (strstr(cam_metadata->camera_name, OV5640_SENSOR_NAME_V1) ||
                           strstr(cam_metadata->camera_name, OV5640_SENSOR_NAME_V2) ||
                           strstr(cam_metadata->camera_name, OV5640_SENSOR_NAME_V3))
-        device = new ImxCamera(id, facing, cam_metadata->orientation, path, cam_copy_hw, cam_csc_hw, jpeg_hw,
-                                                                        cam_metadata);
+        device = new ImxCamera(id, facing, cam_metadata->orientation, path, cam_copy_hw,
+                               cam_csc_hw, jpeg_hw, cam_metadata, subdev_path);
     else if (strstr(cam_metadata->camera_name, UVC_NAME))
         device = UvcDevice::newInstance(id, cam_metadata->camera_name, facing, cam_metadata->orientation, path, cam_copy_hw, cam_csc_hw,
                                jpeg_hw,
