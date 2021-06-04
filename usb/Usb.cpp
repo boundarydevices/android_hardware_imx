d32d990f6854c628020f7eabb7308cd723bff4cf
Gary Bisson <gary.bisson@boundarydevices.com>
2021-06-04 12:22:03 +0200

    usb: check sysfs typec folder for legacy detection
    Our kernel builds the typeC driver which creates the sysfs entry. So
    check whether it is empty or not to truly test typeC presence.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- usb/Usb.cpp
+++ usb/Usb.cpp
@@ -369,8 +369,10 @@ Status getTypeCPortNamesHelper(std::unordered_map<std::string, bool> *names) {
   dp = opendir("/sys/class/typec");
   if (dp != NULL) {
     struct dirent *ep;
+    int nb_nodes = 0;
 
     while ((ep = readdir(dp))) {
+      nb_nodes++;
       if (ep->d_type == DT_LNK) {
         if (std::string::npos == std::string(ep->d_name).find("-partner")) {
           std::unordered_map<std::string, bool>::const_iterator portName =
@@ -384,10 +386,15 @@ Status getTypeCPortNamesHelper(std::unordered_map<std::string, bool> *names) {
       }
     }
     closedir(dp);
+    if (nb_nodes <= 2) {
+      ALOGE("/sys/class/typec dir is empty!");
+      goto error;
+    }
     return Status::SUCCESS;
   }
 
   ALOGE("Failed to open /sys/class/typec");
+error:
   return Status::ERROR;
 }
 
