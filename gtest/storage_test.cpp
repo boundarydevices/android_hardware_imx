21f5fe5177f96861d5e7af3b2ecfb612681a16d5
faqiang.zhu <faqiang.zhu@nxp.com>
2021-12-01 21:09:24 +0800

    MA-17872-1 check udisk mount path based on each enty in the path
    This storage_test now checks whether the udisk mount path in fstab is
    valid. Previously, the mount path is in the form as below, this test
    module erases the last "*" symbol, and checks whether the remaining path
    can be accessed.
    
        /devices/platform/soc@0/32c00000.bus/32e40000.usb/*
    
    On evk_8ulp, the mount path is now as below, regex is used in two
    entries in the path. To make this test module works, it is changed to
    check the path from the begin, access the directory, and read entries in
    that directory, and match with the entry in this path. If this mount
    path can be matched with a real path, then the test succeeds.
    
        /devices/platform/soc@0/29800000.bus/299[02]0000.usb/*
    
    This test has below assumptions:
      1. the mount path starts with "/"
      2. regex is used inner an entry not inter entries.
    
    Change-Id: I95462b037a9b114529594637ab578a2a8f933dea
    Signed-off-by: faqiang.zhu <faqiang.zhu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/17721
    Reviewed-by: Zhai.He <zhai.he@nxp.com>

--- gtest/storage_test.cpp
+++ gtest/storage_test.cpp
@@ -4,14 +4,62 @@
 #include <android-base/properties.h>
 #include <gtest/gtest.h>
 
+#include <string>
+#include <cstring>
+#include <fnmatch.h>
+#include <sys/types.h>
+#include <dirent.h>
+
 using android::fs_mgr::Fstab;
 
 constexpr char kDefaultFstabPath[] = "/vendor/etc/fstab.";
 
+bool udisk_accessiable = false;
+std::string global_pattern;
+
+std::string find_next_sub_pattern(std::string::size_type start_idx)
+{
+    if (start_idx > global_pattern.size()) {
+        return std::string();
+    }
+    std::string chop_head = global_pattern.substr(start_idx);
+    auto end_idx = chop_head.find("/");
+    if (end_idx == std::string::npos) {
+        end_idx = chop_head.size();
+    }
+
+    auto ret = chop_head.substr(0, end_idx);
+    return ret;
+}
+
+void entry_exist_or_not(std::string dir, std::string entry_pattern, std::string::size_type entry_pattern_idx)
+{
+    if (entry_pattern.empty()) {
+        udisk_accessiable = true;
+        return;
+    }
+
+    auto dir_instance = opendir(dir.c_str());
+    if (!dir_instance) {
+        return;
+    }
+
+    struct dirent *dir_entry;
+    std::string next_entry_pattern;
+    while((dir_entry = readdir(dir_instance))) {
+        if(!strncmp(dir_entry->d_name, ".", 1) || !strncmp(dir_entry->d_name, "..", 2))
+            continue;
+
+        if(!fnmatch(entry_pattern.c_str() ,dir_entry->d_name, 0)) {
+            next_entry_pattern = find_next_sub_pattern(entry_pattern_idx + entry_pattern.size() + 1);
+            entry_exist_or_not(dir + dir_entry->d_name + "/", next_entry_pattern, entry_pattern_idx + entry_pattern.size() + 1);
+        }
+    }
+    closedir(dir_instance);
+}
+
 bool usb_disk_mount_point_valid()
 {
-    bool udisk_accessiable = false;
-    std::string udisk_block_device;
     Fstab fstab_entries;
     auto hardware = android::base::GetProperty("ro.hardware", "");
     ReadFstabFromFile(kDefaultFstabPath + hardware, &fstab_entries);
@@ -25,17 +73,16 @@ bool usb_disk_mount_point_valid()
          *    it's a label name give by us.
          */
         if((it.blk_device.find("usb") != it.blk_device.npos) || it.label == "usb") {
-            udisk_block_device = it.blk_device;
+            global_pattern = it.blk_device;
             /* In vold, when usb disk is plugged, vold compares the udisk block device uevent path
              * with the "blk_device" found in the fstab file, with fnmatch() library function.
              *
              * udisk block device uevent is not triggered with this test, and the "blk_device" in
-             * fstab file for udisk now has only one wildcard at the end, remove that wildcard
-             * and check whether it can be accessible.
+             * fstab file for udisk now has regex in each filesystem entry name. iterate on each
+             * entry to see whether the "blk_device" have corresponding path under sysfs.
              */
-            udisk_block_device.erase(udisk_block_device.begin() + (udisk_block_device.size()-1));
-            if( access(("/sys" + udisk_block_device).c_str(), F_OK) == 0)
-                udisk_accessiable = true;
+            auto next_sub_pattern = find_next_sub_pattern(1);
+            entry_exist_or_not(std::string("/sys/"), next_sub_pattern, 1);
         }
     }
 
