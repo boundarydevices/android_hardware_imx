f8781dce8d5ee0afff040011b2a6b83ef7bdf349
Anle Pan <anle.pan@nxp.com>
2022-08-10 15:36:00 +0000

    MA-20345-5 vibrator, fix the build error on android-t-beta
    "libvibratorimximpl" depends on module
    "android.hardware.vibrator-V2-ndk_platform", which not match on
    android-t-beta.
    use "android.hardware.vibrator-V2-ndk_platform" for
    android12, use "android.hardware.vibrator-V2-ndk" for others
    
    Change-Id: Ieae2ac8b6ab5ba74378710e0064563b3ecddc92a
    Signed-off-by: Anle Pan <anle.pan@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/19553
    Reviewed-by: zhang bo <bo.zhang@nxp.com>
    Reviewed-by: dandan.sun <dandan.sun@nxp.com>

---
+++ vibrator/vibrator.go
@@ -0,0 +1,37 @@
+package vibrator
+
+import (
+        "android/soong/android"
+        "android/soong/cc"
+)
+
+func init() {
+    android.RegisterModuleType("vibrator_defaults", vibratorDefaultsFactory)
+}
+
+func vibratorDefaultsFactory() (android.Module) {
+    module := cc.DefaultsFactory()
+    android.AddLoadHook(module, vibratorDefaults)
+    return module
+}
+
+func vibratorDefaults(ctx android.LoadHookContext) {
+    type props struct {
+        Target struct {
+            Android struct {
+                Shared_libs []string
+            }
+        }
+    }
+
+    p := &props{}
+
+    var version string = ctx.AConfig().PlatformVersionName()
+    if(version == "12") {
+        p.Target.Android.Shared_libs = append(p.Target.Android.Shared_libs, "android.hardware.vibrator-V2-ndk_platform");
+    } else {
+        p.Target.Android.Shared_libs = append(p.Target.Android.Shared_libs, "android.hardware.vibrator-V2-ndk")
+    }
+
+    ctx.AppendProperties(p)
+}
