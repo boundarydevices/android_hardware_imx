5193ca3ae13b6fc3c163b117143607b8d4d0e88d
Ondrej Lutera <ondrej.lutera@nxp.com>
2023-02-23 14:11:04 +0100

    AAUTO-715-2 Refine to avoid compiler warnings
    Fixed warning: implicit conversion of NULL constant to 'pthread_t' (aka 'long') [-Wnull-conversion]
    
    Change-Id: I773106e3dd5e82b7ea64de21514c6074d5ca2d3f
    Reviewed-on: http://androidsource.ap.freescale.net/project/20832
    Reviewed-by: Jindong Yue <jindong.yue@nxp.com>

--- alsa/tinyalsa_hal.cpp
+++ alsa/tinyalsa_hal.cpp
@@ -4007,7 +4007,7 @@ static int sco_release_resource(struct imx_audio_device *adev)
     if(adev->tid_sco_rx) {
         adev->b_sco_rx_running = false;
         pthread_join(adev->tid_sco_rx, NULL);
-        adev->tid_sco_rx = NULL;
+        adev->tid_sco_rx = 0;
     }
 
     if(adev->pcm_sco_rx) {
@@ -4024,7 +4024,7 @@ static int sco_release_resource(struct imx_audio_device *adev)
     if(adev->tid_sco_tx) {
         adev->b_sco_tx_running = false;
         pthread_join(adev->tid_sco_tx, NULL);
-        adev->tid_sco_tx = NULL;
+        adev->tid_sco_tx = 0;
     }
 
     if(adev->pcm_sco_tx) {
