05ccebf6fb2faa96a26974c5b96a0e3c6357d8ba
Jindong Yue <jindong.yue@nxp.com>
2023-05-08 15:40:09 +0800

    WCSWREL-435 audio: Clear resampler after release
    BT crash observed in HFP validation while switching
    between output devices like speaker and connected BT device.
    
    The resampler is used only with different input and output
    sample rate. Specifically, speaker/headphone needs resampler,
    while BT doesn't need.
    
    The issue happens when output device switched from
    speaker to BT: the previous resample for speaker is released,
    but not cleard, then BT wrongly uses released resampler.
    Clear released resampler to fix the issue.
    
    Change-Id: I1a30aba0ca42fff405723a0e431f1e82db8ce296
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/21614
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- alsa/tinyalsa_hal.cpp
+++ alsa/tinyalsa_hal.cpp
@@ -1922,6 +1922,7 @@ static int spdif_in_rate_check(struct imx_stream_in *in)
 
             if (in->resampler) {
                 release_resampler(in->resampler);
+                in->resampler = NULL;
             }
 
             if (in->requested_rate != in->config.rate) {
@@ -2019,6 +2020,7 @@ static int start_input_stream(struct imx_stream_in *in)
 
     if (in->resampler) {
         release_resampler(in->resampler);
+        in->resampler = NULL;
     }
     if (in->requested_rate != in->config.rate) {
         in->buf_provider.get_next_buffer = get_next_buffer;
@@ -4471,6 +4473,7 @@ static void adev_close_input_stream(struct audio_hw_device *dev,
 
     if (in->resampler) {
         release_resampler(in->resampler);
+        in->resampler = NULL;
     }
     if (in->proc_buf_in)
         free(in->proc_buf_in);
