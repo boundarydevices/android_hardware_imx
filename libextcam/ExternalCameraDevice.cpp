50cc0f0d0f17a77a348871aa1df1e58ac80d87c5
Anle Pan <anle.pan@nxp.com>
2024-01-18 11:25:13 +0000

    MA-21951 Fix display shakes and tears issue in 424x240 resolution.
    Remove resolutions not 16 bytes aligned in width
    in external camera candidate resolutions.
    These unaligned widths may cause the display
    issue in stress test.
    
    Change-Id: I311b95c486ed45d49c6c2cb96108fcc7b1f5a1fb
    Signed-off-by: Anle Pan <anle.pan@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/23822
    Reviewed-by: Bo Zhang <bo.zhang@nxp.com>

--- libextcam/ExternalCameraDevice.cpp
+++ libextcam/ExternalCameraDevice.cpp
@@ -974,8 +974,9 @@ std::vector<SupportedV4L2Format> ExternalCameraDevice::getCandidateSupportedForm
                     continue;
                 }
 
-                // Discard all formats which is not aligned in height
-                if (frameSize.discrete.height % 4 != 0) {
+                // Discard all formats which is not 16 bytes aligned in width
+                // and not 4 bytes aligned in height
+                if (frameSize.discrete.width % 16 != 0 || frameSize.discrete.height % 4 != 0) {
                     continue;
                 }
 
