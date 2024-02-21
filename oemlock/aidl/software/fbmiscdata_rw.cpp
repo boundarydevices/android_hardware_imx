c458070bd1bd691760bdf5570e224590f36c80e6
Maximus <maximus.sun@nxp.com>
2024-02-21 19:14:10 +0800

    MA-22125 Create software backed oemlock AIDL service
    Add a software backed oemlock AIDL and store the “oem unlocking”
    flag to the “fbmisc” partition instead of the secure storage (rpmb).
    This service startup when Trusty disabled.
    
    Change-Id: If2b4be5265e1cf16d9584930a3829fa95aa56237
    Signed-off-by: Maximus <maximus.sun@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/23991
    Reviewed-by: Ji Luo <ji.luo@nxp.com>

---
+++ oemlock/aidl/software/fbmiscdata_rw.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/logging.h>
+#include <android-base/file.h>
+#include <android-base/properties.h>
+#include <fbmiscdata_rw.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/uio.h>
+
+#define FASTBOOT_PARTITION_FBMISC "/dev/block/by-name/fbmisc"
+
+fbmiscError fbmiscDataBlockLock::readDeviceUnlockPermission(uint8_t* status) {
+    int file_fd = -1;
+    std::string mDataBlockFile;
+    ssize_t ret = -1;
+
+    mDataBlockFile = FASTBOOT_PARTITION_FBMISC;
+
+    file_fd = open(mDataBlockFile.c_str(), O_RDWR);
+    if (file_fd < 0) {
+        LOG(INFO) << "OemLock: can not open: " << mDataBlockFile;
+        return fbmiscError::FBMISC_ERROR_INTERNAL;
+    }
+
+    lseek(file_fd, -1, SEEK_END);
+
+    ret = read(file_fd, status, 1);
+
+    close(file_fd);
+
+    if (ret == 1)
+        return fbmiscError::FBMISC_ERROR_NONE;
+    else
+        return fbmiscError::FBMISC_ERROR_INTERNAL;
+}
+
+fbmiscError fbmiscDataBlockLock::writeDeviceUnlockPermission(uint8_t status) {
+    int file_fd = -1;
+    std::string mDataBlockFile;
+    ssize_t ret = -1;
+
+    mDataBlockFile = FASTBOOT_PARTITION_FBMISC;
+
+    file_fd = open(mDataBlockFile.c_str(), O_RDWR);
+    if (file_fd < 0) {
+        LOG(INFO) << "OemLock: can not open: " << mDataBlockFile;
+        return fbmiscError::FBMISC_ERROR_INTERNAL;
+    }
+
+    lseek(file_fd, -1, SEEK_END);
+
+    ret = write(file_fd, &status, 1);
+
+    close(file_fd);
+
+    if (ret == 1)
+        return fbmiscError::FBMISC_ERROR_NONE;
+    else
+        return fbmiscError::FBMISC_ERROR_INTERNAL;
+}
