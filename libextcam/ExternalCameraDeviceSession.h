ffa243873fc96d7267558a57bfcda327971e06e3
Fang Hui <hui.fang@nxp.com>
2024-04-02 23:52:49 +0800

    Revert "MA-21555-3 Use NV12 as inter buffer format for external camera"
    This reverts commit c9b846db90cae0e9c2d2c6fc2dd1b8e804122e77.
    
    The code is no need due to
    MA-22270(ExtCamHAL: Support encode JPEG from NV12/NV16/YUYV).
    
    Change-Id: I6104c011cfadd7bf97b424c0c6693990d4b9c16d
    Reviewed-on: http://androidsource.nxp.com/project/24368
    Reviewed-by: Anle Pan <anle.pan@nxp.com>

--- libextcam/ExternalCameraDeviceSession.h
+++ libextcam/ExternalCameraDeviceSession.h
@@ -255,9 +255,6 @@ public:
         std::unordered_map<Size, std::shared_ptr<AllocatedFrame>, SizeHasher> mScaledYu12Frames;
         YCbCrLayout mYu12FrameLayout;
         YCbCrLayout mYu12ThumbFrameLayout;
-        std::shared_ptr<AllocatedFrame> mI420Frame; // If media buffer format (mYu12Frame) is NV12,
-                                                    // convert to I420, then encode jpeg.
-        YCbCrLayout mI420FrameLayout;
         std::vector<uint8_t> mMuteTestPatternFrame;
         uint32_t mTestPatternData[4] = {0, 0, 0, 0};
         bool mCameraMuted = false;
