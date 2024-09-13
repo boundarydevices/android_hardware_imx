244c2a4f12ffbdda0f44a1d9ac148b5d0d68df7a
Jindong Yue <jindong.yue@nxp.com>
2024-09-13 14:42:44 +0900

    MA-22689-5 audio: Remove emulator code in transfer
    Remove those code for emulator to avoid unnecessary sleeps.
    
    Change-Id: I417bc6e8c62bf36070542ff81158d58004ea6cb8
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/25971
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- audio/default/primary/StreamPrimary.cpp
+++ audio/default/primary/StreamPrimary.cpp
@@ -58,36 +58,8 @@ StreamPrimary::StreamPrimary(StreamContext* context, const Metadata& metadata)
 
 ::android::status_t StreamPrimary::transfer(void* buffer, size_t frameCount,
                                             size_t* actualFrameCount, int32_t* latencyMs) {
-    // This is a workaround for the emulator implementation which has a host-side buffer
-    // and is not being able to achieve real-time behavior similar to ADSPs (b/302587331).
-    if (!mSkipNextTransfer) {
-        RETURN_STATUS_IF_ERROR(
-                StreamAlsa::transfer(buffer, frameCount, actualFrameCount, latencyMs));
-    } else {
-        LOG(DEBUG) << __func__ << ": skipping transfer (" << frameCount << " frames)";
-        *actualFrameCount = frameCount;
-        if (mIsInput) memset(buffer, 0, frameCount * mFrameSizeBytes);
-        mSkipNextTransfer = false;
-    }
-    if (!mIsAsynchronous) {
-        const long bufferDurationUs =
-                (*actualFrameCount) * MICROS_PER_SECOND / mContext.getSampleRate();
-        const auto totalDurationUs =
-                (::android::uptimeNanos() - mStartTimeNs) / NANOS_PER_MICROSECOND;
-        mFramesSinceStart += *actualFrameCount;
-        const long totalOffsetUs =
-                mFramesSinceStart * MICROS_PER_SECOND / mContext.getSampleRate() - totalDurationUs;
-        LOG(VERBOSE) << __func__ << ": totalOffsetUs " << totalOffsetUs;
-        if (totalOffsetUs > 0) {
-            const long sleepTimeUs = std::min(totalOffsetUs, bufferDurationUs);
-            LOG(VERBOSE) << __func__ << ": sleeping for " << sleepTimeUs << " us";
-            usleep(sleepTimeUs);
-        } else {
-            mSkipNextTransfer = true;
-        }
-    } else {
-        LOG(VERBOSE) << __func__ << ": asynchronous transfer";
-    }
+    RETURN_STATUS_IF_ERROR(
+            StreamAlsa::transfer(buffer, frameCount, actualFrameCount, latencyMs));
     return ::android::OK;
 }
 
