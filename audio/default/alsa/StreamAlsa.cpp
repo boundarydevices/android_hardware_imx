573be8323c88e1efe6bc30673af78ce83f14c124
Jindong Yue <jindong.yue@nxp.com>
2024-10-23 11:10:52 +0800

    MA-22962-1 audio: Add PCM pause and resume
    In LPA case, software pause causes long delays
    due to large buffers.
    This change mitigates the delay by leveraging
    hardware-controlled pause/resume.
    
    Change-Id: I1149ffcfb45cbf5360334cac9b90907a755cd86f
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/26184
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- audio/default/alsa/StreamAlsa.cpp
+++ audio/default/alsa/StreamAlsa.cpp
@@ -60,6 +60,7 @@ StreamAlsa::~StreamAlsa() {
 }
 
 ::android::status_t StreamAlsa::pause() {
+    proxy_pause(mAlsaDeviceProxies[0].get());
     return ::android::OK;
 }
 
@@ -71,6 +72,7 @@ StreamAlsa::~StreamAlsa() {
 ::android::status_t StreamAlsa::start() {
     if (!mAlsaDeviceProxies.empty()) {
         // This is a resume after a pause.
+        proxy_resume(mAlsaDeviceProxies[0].get());
         return ::android::OK;
     }
     decltype(mAlsaDeviceProxies) alsaDeviceProxies;
