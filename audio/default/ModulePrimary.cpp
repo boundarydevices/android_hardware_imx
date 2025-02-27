dd000699b580e80561551c1a90556ebbb667c284
Jindong Yue <jindong.yue@nxp.com>
2025-02-27 16:13:31 +0800

    MA-23284-1 audio: Increase nominal latency to disable FastMixer and FastCapture
    The FAST thread has higher requirements on audio latency,
    currently it is not suitable to enable them.
    
    Change-Id: Ia2e85104a925b1a50681ffd8bdf95dcd449f61a0
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/27637
    Reviewed-by: Hui Fang <hui.fang@nxp.com>

--- audio/default/ModulePrimary.cpp
+++ audio/default/ModulePrimary.cpp
@@ -82,7 +82,7 @@ ndk::ScopedAStatus ModulePrimary::createOutputStream(
 }
 
 int32_t ModulePrimary::getNominalLatencyMs(const AudioPortConfig&) {
-    static constexpr int32_t kLatencyMs = 10;
+    static constexpr int32_t kLatencyMs = 16;
     return kLatencyMs;
 }
 
